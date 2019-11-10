9bc1d228825cb21babbd89e06297486c92d0f6ea
hololee <lccandol@naver.com>
2019-11-10 19:46:31 +0900

    update

--- assignment_cnn.py
+++ assignment_cnn.py
@@ -1,9 +1,9 @@
 import numpy as np
 # my class
-import fcn.model
-import fcn.tools as tool
-from fcn.model import NeuralNetwork as network
-from fcn.datas import DataManager as data_manager
+import cnn.model
+# import cnn.tools as tool
+from cnn.model import ConvolutionalNetwork as network
+from cnn.datas import DataManager as data_manager
 
 # data stack for plotting.
 train_acc = []
@@ -12,175 +12,83 @@ valid_acc = []
 valid_err = []
 
 # config list.
-INFO_SIGMOID_MOMENTUM_MSE_BATCH = {'total_epoch': 3000,
-                                   'batch_size': 60000,
-                                   'learning_rate': 1e-6,
-                                   'random_seed': 42,
-                                   'train_dataset_size': 60000,
-                                   'test_dataset_size': 10000,
-                                   'momentum': 0.8,
-                                   'optimizer': fcn.model.OPTIMIZER_GD_MOMENTUM,
-                                   'activation': fcn.model.ACTIVATE_SIGMOID,
-                                   'loss': fcn.model.LOSS_MSE}
-
-INFO_RELU_GD_MSE_BATCH = {'total_epoch': 100,
-                          'batch_size': 60000,
-                          'learning_rate': 1e-6,
-                          'random_seed': 42,
-                          'train_dataset_size': 60000,
-                          'test_dataset_size': 10000,
-                          'momentum': 0.8,
-                          'optimizer': fcn.model.OPTIMIZER_GD_MOMENTUM,
-                          'activation': fcn.model.ACTIVATE_RELU,
-                          'loss': fcn.model.LOSS_MSE}
-
-INFO_RELU_GD_MSE_MINIBATCH = {'total_epoch': 100,
-                              'batch_size': 1000,
-                              'learning_rate': 1e-6,
-                              'random_seed': 42,
-                              'train_dataset_size': 60000,
-                              'test_dataset_size': 10000,
-                              'momentum': 0.8,
-                              'optimizer': fcn.model.OPTIMIZER_GD_MOMENTUM,
-                              'activation': fcn.model.ACTIVATE_RELU,
-                              'loss': fcn.model.LOSS_MSE}
-
-INFO_RELU_GD_MSE_STOCHASTIC = {'total_epoch': 50,
-                               'batch_size': 1,
-                               'learning_rate': 1e-6,
-                               'random_seed': 42,
-                               'train_dataset_size': 60000,
-                               'test_dataset_size': 10000,
-                               'momentum': 0.8,
-                               'optimizer': fcn.model.OPTIMIZER_GD_MOMENTUM,
-                               'activation': fcn.model.ACTIVATE_RELU,
-                               'loss': fcn.model.LOSS_MSE}
-
-# set config.
-current_config = INFO_RELU_GD_MSE_STOCHASTIC
-
-#
-# config_assignmentA_MOMENTUM_CROSSENTROPY = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1,
-#                                             'random_seed': 42,
-#                                             'train_dataset_size': 60000, 'test_dataset_size': 10000, 'momentum': 0.9,
-#                                             'optimizer': fcn.model.OPTIMIZER_GD_MOMENTUM,
-#                                             'activation': fcn.model.ACTIVATE_SIGMOID, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentA_CROSSENTROPY = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
-#                                    'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                    'optimizer': fcn.model.OPTIMIZER_GD,
-#                                    'activation': fcn.model.ACTIVATE_SIGMOID, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentB_CROSSENTROPY = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1, 'random_seed': 42,
-#                                    'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                    'optimizer': fcn.model.OPTIMIZER_GD,
-#                                    'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentC_MINI_BATCH_CROSSENTROPY = {'total_epoch': 50, 'batch_size': 1000, 'learning_rate': 0.1,
-#                                               'random_seed': 42,
-#                                               'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                               'optimizer': fcn.model.OPTIMIZER_GD,
-#                                               'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentC_BATCH_CROSSENTROPY = {'total_epoch': 80, 'batch_size': 60000, 'learning_rate': 0.1,
-#                                          'random_seed': 42,
-#                                          'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                          'optimizer': fcn.model.OPTIMIZER_GD,
-#                                          'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentC_STOCHASTIC_CROSSENTROPY = {'total_epoch': 10, 'batch_size': 1, 'learning_rate': 0.01,
-#                                               'random_seed': 42,
-#                                               'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                               'optimizer': fcn.model.OPTIMIZER_GD,
-#                                               'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY}
-#
-# config_assignmentD_ADAGRAD_CROSSENTROPY = {'total_epoch': 30, 'batch_size': 1000, 'learning_rate': 0.005,
-#                                            'random_seed': 42,
-#                                            'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                            'optimizer': fcn.model.OPTIMIZER_ADAGRAD,
-#                                            'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY,
-#                                            'epsilon': 1e-5}
-#
-# config_assignmentD_ADAM_CROSSENTROPY = {'total_epoch': 30, 'batch_size': 1000, 'learning_rate': 0.005,
-#                                         'random_seed': 42,
-#                                         'train_dataset_size': 60000, 'test_dataset_size': 10000,
-#                                         'optimizer': fcn.model.OPTIMIZER_ADAM,
-#                                         'activation': fcn.model.ACTIVATE_RELU, 'loss': fcn.model.LOSS_CROSSENTROPY,
-#                                         'beta1': 0.9,
-#                                         'beta2': 0.999,
-#                                         'epsilon': 1e-8}
+CONFIG = {'total_epoch': 3000,
+          'batch_size': 60000,
+          'learning_rate': 1e-6,
+          'train_dataset_size': 60000,
+          'test_dataset_size': 10000}
 
 # define network fcn.
-network_model = network(configure=current_config, h1=256, h2=256)
+network_model = network(configure=CONFIG, filter_size=3, d1=16, d2=32, h1=64)
 dataManager = data_manager()
 
-# fix the random value.
-# np.random.seed(network_model.SEED)
+network_model.train(dataManager.test_X[0:2], dataManager.test_y[0:2])
 
-# using mini-batch
-for i in range(network_model.TOTAL_EPOCH):
-    print("============== EPOCH {} START ==============".format(i + 1))
-    for j in range(dataManager.train_dataset_size // network_model.BATCH_SIZE):
-        # print("-------------- batch {} training...".format(j))
-
-        # load batch data.
-        batch_x, batch_y = dataManager.next_batch(network_model.BATCH_SIZE)
-
-        # train model.
-        network_model.train(batch_x, batch_y)
-
-        if current_config["batch_size"] == 1:
-            if j % 100 == 0:
-                # save data.
-                # calculate accuracy and loss
-                output_train = network_model.predict(dataManager.X_train)
-                accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
-
-                # add data to stack
-                train_acc.append(accuracy_train)
-                train_err.append(loss_train)
-
-                # calculate test dataset.
-                output_test = network_model.predict(dataManager.X_test)
-                accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
-
-                # add data to stack
-                valid_acc.append(accuracy_test)
-                valid_err.append(loss_test)
-        else:
-            if j % 10 == 0:
-                # save data.
-                # calculate accuracy and loss
-                output_train = network_model.predict(dataManager.X_train)
-                accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
-
-                # add data to stack
-                train_acc.append(accuracy_train)
-                train_err.append(loss_train)
-
-                # calculate test dataset.
-                output_test = network_model.predict(dataManager.X_test)
-                accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
-
-                # add data to stack
-                valid_acc.append(accuracy_test)
-                valid_err.append(loss_test)
-    print("============== EPOCH {} END ================".format(i + 1))
-
-    # shake data when epoch ended.
-    # dataManager.shake_data()
-
-    # calculate accuracy and loss
-    output_train = network_model.predict(dataManager.X_train)
-    accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
-
-    # calculate test dataset.
-    output_test = network_model.predict(dataManager.X_test)
-    accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
-
-    print("train accuracy : {:.4}; loss : {:.3}, test accuracy : {:.3}; loss : {:.3}".format(accuracy_train, loss_train,
-                                                                                             accuracy_test, loss_test))
-
-    if i % 10 == 0 or i == network_model.TOTAL_EPOCH - 1:
-        # draw graph.
-        tool.plotting(current_config['learning_rate'], train_acc, train_err, valid_acc, valid_err)
+#
+# # using mini-batch
+# for i in range(network_model.TOTAL_EPOCH):
+#     print("============== EPOCH {} START ==============".format(i + 1))
+#     for j in range(dataManager.train_dataset_size // network_model.BATCH_SIZE):
+#         # print("-------------- batch {} training...".format(j))
+#
+#         # load batch data.
+#         batch_x, batch_y = dataManager.next_batch(network_model.BATCH_SIZE)
+#
+#         # train model.
+#         network_model.train(batch_x, batch_y)
+#
+#         if current_config["batch_size"] == 1:
+#             if j % 100 == 0:
+#                 # save data.
+#                 # calculate accuracy and loss
+#                 output_train = network_model.predict(dataManager.X_train)
+#                 accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
+#
+#                 # add data to stack
+#                 train_acc.append(accuracy_train)
+#                 train_err.append(loss_train)
+#
+#                 # calculate test dataset.
+#                 output_test = network_model.predict(dataManager.X_test)
+#                 accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
+#
+#                 # add data to stack
+#                 valid_acc.append(accuracy_test)
+#                 valid_err.append(loss_test)
+#         else:
+#             if j % 10 == 0:
+#                 # save data.
+#                 # calculate accuracy and loss
+#                 output_train = network_model.predict(dataManager.X_train)
+#                 accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
+#
+#                 # add data to stack
+#                 train_acc.append(accuracy_train)
+#                 train_err.append(loss_train)
+#
+#                 # calculate test dataset.
+#                 output_test = network_model.predict(dataManager.X_test)
+#                 accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
+#
+#                 # add data to stack
+#                 valid_acc.append(accuracy_test)
+#                 valid_err.append(loss_test)
+#     print("============== EPOCH {} END ================".format(i + 1))
+#
+#     # shake data when epoch ended.
+#     # dataManager.shake_data()
+#
+#     # calculate accuracy and loss
+#     output_train = network_model.predict(dataManager.X_train)
+#     accuracy_train, loss_train = network_model.getAccuracyAndLoss(output_train, dataManager.y_train)
+#
+#     # calculate test dataset.
+#     output_test = network_model.predict(dataManager.X_test)
+#     accuracy_test, loss_test = network_model.getAccuracyAndLoss(output_test, dataManager.y_test)
+#
+#     print("train accuracy : {:.4}; loss : {:.3}, test accuracy : {:.3}; loss : {:.3}".format(accuracy_train, loss_train,
+#                                                                                              accuracy_test, loss_test))
+#
+#     if i % 10 == 0 or i == network_model.TOTAL_EPOCH - 1:
+#         # draw graph.
+#         tool.plotting(current_config['learning_rate'], train_acc, train_err, valid_acc, valid_err)
--- cnn/model.py
+++ cnn/model.py
@@ -3,12 +3,28 @@ import numpy as np
 
 class ConvolutionalNetwork:
 
-    def __init__(self):
+    def __init__(self, configure, filter_size=3, d1=16, d2=32, h1=64):
+
+        # get configure.
+        self.TOTAL_EPOCH = configure['total_epoch']
+        self.BATCH_SIZE = configure['batch_size']
+        self.LEARNING_RATE = configure['learning_rate']
+        self.TRAIN_DATASET_SIZE = configure['train_dataset_size']
+        self.TEST_DATASET_SIZE = configure['test_dataset_size']
+
         # outdim, indim, h, w
-        self.conv1 = np.random.randn(3, 3, 3, 16) / np.sqrt(3)
-        self.conv2 = np.random.randn(3, 3, 16, 32) / np.sqrt(16)
-        self.w3 = np.random.randn(8 * 8 * 32, 64) / np.sqrt(32)
-        self.w4 = np.random.randn(64, 10) / np.sqrt(64)
+        self.w1 = np.random.randn(filter_size, filter_size, 3, d1) / np.sqrt(3)
+        self.b1 = np.random.randn(d1) / np.sqrt(3)
+
+        self.w2 = np.random.randn(filter_size, filter_size, d1, d2) / np.sqrt(d1)
+        self.b2 = np.random.randn(d2) / np.sqrt(d1)
+
+        # 8 means 32 - > 16 (pooling) , 16 -> 8 (pooling)
+        self.w3 = np.random.randn(8 * 8 * d2, h1) / np.sqrt(d2)
+        self.b3 = np.random.randn(h1) / np.sqrt(d2)
+
+        self.w4 = np.random.randn(h1, 10) / np.sqrt(h1)
+        self.b4 = np.random.randn(10) / np.sqrt(h1)
 
     def relu(self, x):
         array = np.copy(x)
@@ -28,10 +44,6 @@ class ConvolutionalNetwork:
         exps = np.exp(array)
         return exps / np.sum(exps, axis=1).reshape([exps.shape[0], 1])
 
-    def padding(self, x):
-        # x.shape = (N, H ,W ,C)
-        return np.pad(x, [(0, 0), (1, 1), (1, 1), (0, 0)], 'constant')
-
     def Im2Col(self, input_data, filter_h, filter_w, stride=1, pad=0):
         N, H, W, C = input_data.shape
         out_h = (H + 2 * pad - filter_h) // stride + 1
@@ -44,28 +56,162 @@ class ConvolutionalNetwork:
             y_max = y + stride * out_h
             for x in range(filter_w):
                 x_max = x + stride * out_w
-                col[:, :, y, x, :, :] = img[:, y:y_max:stride, x:x_max:stride, :]
+                # NOTICE : transpose for calculation.
+                col[:, :, y, x, :, :] = np.transpose(img[:, y:y_max:stride, x:x_max:stride, :], [0, 3, 1, 2])
 
-        col = col.transpose([0, 4, 5, 1, 2, 3]).reshape(N * out_h * out_w, -1)
+        col = np.reshape(col.transpose([0, 4, 5, 1, 2, 3]), [N * out_h * out_w, -1])
         return col
 
     def Col2Im(self, col, input_shape, filter_h, filter_w, stride=1, pad=0):
         N, H, W, C = input_shape
         out_h = (H + 2 * pad - filter_h) // stride + 1
         out_w = (W + 2 * pad - filter_w) // stride + 1
-        col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose([0, 3, 4, 5, 1, 2])
+        col = np.reshape(col, newshape=[N, out_h, out_w, C, filter_h, filter_w]).transpose([0, 3, 4, 5, 1, 2])
 
         img = np.zeros((N, H + 2 * pad + stride - 1, W + 2 * pad + stride - 1, C))
+
+        # NOTICE : transpose for calculation.
+        img = np.transpose(img, [0, 3, 1, 2])
+
         for y in range(filter_h):
             y_max = y + stride * out_h
             for x in range(filter_w):
                 x_max = x + stride * out_w
-                img[:, y:y_max:stride, x:x_max:stride, :] += col[:, :, y, x, :, :]
+                img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+        # NOTICE : transpose for calculation.
+        img = np.transpose(img, [0, 2, 3, 1])
 
         return img[:, pad:H + pad, pad:W + pad, :]
 
-    def conv(self, x, weight, stride=1, padding=0):
+    def conv(self, x, weight, bias, stride=1, padding=0):
         filter_h, filter_w, filter_c, filter_d = weight.shape
         N, H, W, C = x.shape
 
-        out_height = (1 + (H + 2 * padding - filter_h) / stride)
+        out_height = int((H + 2 * padding - filter_h) / stride + 1)
+        out_width = int((W + 2 * padding - filter_w) / stride + 1)
+
+        flatten_img = self.Im2Col(x, filter_h, filter_w, stride, padding)
+        flatten_weight = weight.reshape(filter_d, -1).T
+        out = np.dot(flatten_img, flatten_weight) + bias
+        out = np.reshape(out, [N, out_height, out_width, -1])
+
+        return out, flatten_img
+
+    def max_pool(self, x, pooling_size=2, stride=2, padding=0):
+        N, H, W, C = x.shape
+        out_height = int((H - pooling_size) / stride + 1)
+        out_width = int((W - pooling_size) / stride + 1)
+
+        flatten_img = self.Im2Col(x, pooling_size, pooling_size, stride, padding)
+        # reshape for max.
+        flatten_img = flatten_img.reshape(-1, pooling_size * pooling_size)
+
+        # find max val.
+        out = np.max(flatten_img, axis=-1)
+        # save the max val location.
+        loc = np.argmax(flatten_img, axis=-1)
+        out = np.reshape(out, [N, out_height, out_width, C])
+
+        return out, loc
+
+    def flat_img(self, x):
+        return x.reshape(x.shape[0], -1)
+
+    def FeedForward(self, x):
+
+        y1, flatten_y1 = self.conv(x, self.w1, self.b1, stride=1, padding=1)
+        activated_y1 = self.relu(y1)
+        pooling_y1, max_loc_y1 = self.max_pool(activated_y1)
+
+        y2, flatten_y2 = self.conv(pooling_y1, self.w2, self.b2, stride=1, padding=1)
+        activated_y2 = self.relu(y2)
+        pooling_y2, max_loc_y2 = self.max_pool(activated_y2)
+
+        # flat image
+        y3 = np.dot(self.flat_img(pooling_y2), self.w3) + self.b3
+        activated_y3 = self.relu(y3)
+
+        y4 = np.dot(activated_y3, self.w4) + self.b4
+        result = self.softmax(y4)
+
+        return activated_y1, activated_y2, activated_y3, result, max_loc_y1, max_loc_y2
+
+    def backpropagation(self, x, labelY, out1, out2, out3, out4, max_loc_y1, max_loc_y2, flatten_y1, flatten_y2):
+
+        # POINT : soft max layer.
+        #
+        # NOTICE : softmax back.
+        error_back = (out4 - labelY) / len(x)  # divided by batch size.
+
+        # POINT : fully connected layer.
+        #
+        # NOTICE : fully weight 4 back.
+        d_w4 = np.dot(out3.T, error_back)
+        d_b4 = np.sum(error_back, axis=0)
+        error_back = np.dot(error_back, self.w4.T)
+
+        # NOTICE : ReLU back.
+        error_back = self.back_relu(out3)
+        # NOTICE : fully weight 3 back.
+        # output x is 4d array, so change the shape.
+        d_w3 = np.dot(out2.reshape(x.shape[0], -1).T, error_back)
+        d_b3 = np.sum(error_back, axis=0)
+        error_back = np.dot(error_back, self.w3.T)
+
+        # POINT : convolution layer.
+        #
+        # NOTICE : pooling back.
+        # un flat.
+        error_back = error_back.reshape([-1, 8, 8, 32])
+        # flated pooling
+        flated_pool = np.zeros((error_back.size, 4))
+        flated_pool[np.arange(len(max_loc_y2)), max_loc_y2.flatten()] = error_back.flatten()
+        flated_pool = flated_pool.reshape(error_back.shape + (4,))
+        flated_pool = flated_pool.reshape(flated_pool.shape[0] * flated_pool.shape[1] * flated_pool.shape[2], -1)
+        error_back = self.Col2Im(flated_pool, [len(error_back), 16, 16, 32], 2, 2, stride=2, pad=0)
+
+        # NOTICE: Relu back.
+        error_back = self.back_relu(out2)*error_back ################33333333
+        # NOTICE : convolution 2 back.
+        error_back = error_back.reshape([-1, len(self.w2)])
+        d_w2 = np.dot(flatten_y2.T, error_back)
+        d_w2 = np.transpose(d_w2.transpose([1, 0]).reshape([32, 16, 3, 3]), [0, 2, 3, 1])
+        d_b2 = np.sum(error_back, axis=0)
+
+        # NOTICE : pooling back.
+        # un flat.
+        error_back = error_back.reshape([-1, 16, 16, 16])
+        # flated pooling
+        flated_pool = np.zeros((error_back.size, 4))
+        flated_pool[np.arange(len(max_loc_y2)), max_loc_y2.flatten()] = error_back.flatten()
+        flated_pool = flated_pool.reshape(error_back.shape + (4,))
+        flated_pool = flated_pool.reshape(flated_pool.shape[0] * flated_pool.shape[1] * flated_pool.shape[2], -1)
+        error_back = self.Col2Im(flated_pool, [len(error_back), 32, 32, 16], 2, 2, stride=2, pad=0)
+        # channel is not changed.
+
+        # NOTICE: Relu back.
+        error_back = self.back_relu(out2)*error_back ################33333333
+        # NOTICE : convolution 2 back.
+        error_back = error_back.reshape([-1, len(self.w2)])
+        d_w2 = np.dot(flatten_y2.T, error_back)
+        d_w2 = np.transpose(d_w2.transpose([1, 0]).reshape([32, 16, 3, 3]), [0, 2, 3, 1])
+        d_b2 = np.sum(error_back, axis=0)
+
+        pass
+        # return d_w1, d_w2, d_w3, d_w4, d_b1, d_b2, d_b3, d_b4
+
+    # train model.
+    def train(self, x, y):
+        activated_y1, activated_y2, activated_y3, result, max_loc_y1, max_loc_y2 = self.FeedForward(x)
+        self.backpropagation(x, y, activated_y1, activated_y2, activated_y3, result, max_loc_y1, max_loc_y2)
+
+        pass
+
+    def getAccuracyAndLoss(self, output_of_model, output):
+        accuracy = np.mean(np.equal(np.argmax(output_of_model, axis=1), np.argmax(output, axis=1)))
+
+        # cross entropy loss
+        loss = -np.mean(output * np.log(output_of_model) + (1 - output) * np.log(1 - output_of_model))
+
+        return accuracy, loss
